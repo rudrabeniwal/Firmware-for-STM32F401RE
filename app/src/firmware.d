889990165f5474a4902eda5b3dbb68128acccd02
Rudra Beniwal <rudrabeniwal@icloud.com>
2024-06-14 16:16:06 +0530

    digging deep in interrupts and memory mapping

---
+++ app/src/firmware.d
@@ -0,0 +1,29 @@
+src/firmware.o: src/firmware.c \
+ ../libopencm3/include/libopencm3/stm32/rcc.h \
+ ../libopencm3/include/libopencm3/cm3/common.h \
+ C:/Program\ Files\ (x86)/Arm\ GNU\ Toolchain\ arm-none-eabi/13.2\ Rel1/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ C:/Program\ Files\ (x86)/Arm\ GNU\ Toolchain\ arm-none-eabi/13.2\ Rel1/arm-none-eabi/include/stdint.h \
+ C:/Program\ Files\ (x86)/Arm\ GNU\ Toolchain\ arm-none-eabi/13.2\ Rel1/arm-none-eabi/include/machine/_default_types.h \
+ C:/Program\ Files\ (x86)/Arm\ GNU\ Toolchain\ arm-none-eabi/13.2\ Rel1/arm-none-eabi/include/sys/features.h \
+ C:/Program\ Files\ (x86)/Arm\ GNU\ Toolchain\ arm-none-eabi/13.2\ Rel1/arm-none-eabi/include/_newlib_version.h \
+ C:/Program\ Files\ (x86)/Arm\ GNU\ Toolchain\ arm-none-eabi/13.2\ Rel1/arm-none-eabi/include/sys/_intsup.h \
+ C:/Program\ Files\ (x86)/Arm\ GNU\ Toolchain\ arm-none-eabi/13.2\ Rel1/arm-none-eabi/include/sys/_stdint.h \
+ C:/Program\ Files\ (x86)/Arm\ GNU\ Toolchain\ arm-none-eabi/13.2\ Rel1/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
+ ../libopencm3/include/libopencm3/stm32/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/f4/memorymap.h \
+ ../libopencm3/include/libopencm3/cm3/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/f4/rcc.h \
+ ../libopencm3/include/libopencm3/stm32/pwr.h \
+ ../libopencm3/include/libopencm3/stm32/f4/pwr.h \
+ ../libopencm3/include/libopencm3/stm32/common/pwr_common_v1.h \
+ ../libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ ../libopencm3/include/libopencm3/stm32/gpio.h \
+ ../libopencm3/include/libopencm3/stm32/f4/gpio.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_f24.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_f234.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_all.h \
+ ../libopencm3/include/libopencm3/cm3/systick.h \
+ ../libopencm3/include/libopencm3/cm3/vector.h \
+ ../libopencm3/include/libopencm3/cm3/nvic.h \
+ ../libopencm3/include/libopencm3/dispatch/nvic.h \
+ ../libopencm3/include/libopencm3/stm32/f4/nvic.h
